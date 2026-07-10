b3ab12119ad328249f47fca0e85728619f7971fa
fts18 <anandsagardubey71568@gmail.com>
2026-07-10 10:45:25 +0530

    Enforce single-instance policy using Mutex

--- windows/Program.cs
+++ windows/Program.cs
@@ -2,12 +2,25 @@ namespace BitBar;
 
 static class Program
 {
+    private static System.Threading.Mutex? mutex = null;
+
     /// <summary>
     ///  The main entry point for the application.
     /// </summary>
     [STAThread]
     static void Main()
     {
+        const string appName = "Global\\BitBarNativeNetMonitor";
+        bool createdNew;
+
+        mutex = new System.Threading.Mutex(true, appName, out createdNew);
+
+        if (!createdNew)
+        {
+            // Application is already running. Exit immediately.
+            return;
+        }
+
         // To customize application configuration such as set high DPI settings or default font,
         // see https://aka.ms/applicationconfiguration.
         ApplicationConfiguration.Initialize();
